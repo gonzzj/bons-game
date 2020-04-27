77833d2edf5990e7e6a1c39440f2a5938ec54552
Gonzalo Jusid <gonzalo.jusid@despegar.com>
2020-04-27 20:36:46 -0300

    Fixed login button 2

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './app';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <App />,
     document.getElementById('root')
 );
 
